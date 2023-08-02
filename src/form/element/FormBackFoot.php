553f27708d93dbdf03c328f18cbe8d92d0b7c9e0
xqkeji <xqkeji@foxmail.com>
2023-08-02 09:36:17 +0800

    src/form/element/FormBackFoot.php 增加表单底部只有一个返回按钮

---
+++ src/form/element/FormBackFoot.php
@@ -0,0 +1,20 @@
+<?php
+return [
+	'form_foot',
+	'attr_class'=>'form-group row',
+	[
+		[
+			'div',
+			'attr_class'=>'col-4 text-start',
+			[
+				[
+					'button',
+					'name'=>'back',
+					'attr_class'=>'btn btn-secondary xq-backpage',
+					'attr_value'=>'返回',
+					'attr_style'=>'width:100px;margin-bottom:1rem;',
+				]
+			],
+		],
+	],
+];
