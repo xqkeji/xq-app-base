82601d6972f4f8232a243ca496a3849ce8e91592
xqkeji <xqkeji@foxmail.com>
2023-08-04 18:53:01 +0800

    添加ordernum表单元素

---
+++ src/form/element/ListOrdernum.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'ListItem',
+	'name'=>'ordernum',
+	'text'=>'序号',
+	'attr_style'=>'width:80px;'
+];
---
+++ src/form/element/Ordernum.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'number',
+    'name'=>'ordernum',
+    'text'=>'序号',
+    'attr_required'=>'true',
+    'attr_style'=>'width:80px;',
+    'validators'=>[['required']],
+    'filters'=>['int'],
+];
