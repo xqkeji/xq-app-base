6abbd6c5011dafd13eb68fbe2975300ddf8c2b22
xqkeji <xqkeji@foxmail.com>
2023-08-05 12:55:16 +0800

    添加列表删除表单元素

---
+++ src/form/element/ListDelete.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	'ListItem',
+	'name'=>'operation',
+	'text'=>'操作',
+	'attr_style'=>'width:80px;',
+	[
+		[
+			'button',
+			'attr_class'=>'btn btn-danger btn-sm xq-delete',
+			'attr_value'=>'删除',	
+		],
+	],
+];
