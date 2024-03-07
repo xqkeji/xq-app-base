d6678e5a6f238bdc091c0a0528da5f658c5780f9
xqkeji <xqkeji@foxmail.com>
2024-03-07 10:47:57 +0800

    更新

---
+++ src/form/element/Submit.php
@@ -0,0 +1,20 @@
+<?php
+return [
+	'div',
+	'attr_class'=>'form-group row',
+	[
+		[
+			'div',
+			'attr_class'=>'col-8 text-center',
+			[
+				[
+					'submit',
+					'name'=>'submit',
+					'attr_class'=>'btn btn-primary',
+					'attr_style'=>'width:100px;margin-bottom:1rem;',
+					'attr_value'=>'确定',
+				]
+			],
+		],
+	],
+];
--- src/form/element/Username.php
+++ src/form/element/Username.php
@@ -3,6 +3,7 @@ return [
 	'text',
 	'text'=>'用户名',
 	'name'=>'username',
+	'placeholder'=>'请输入用户名',
 	'attr_required'=>'1',
 	'validators'=>[
 		['required']
