3d4d1f0990a3ab5b9b3eff65cbcca18625397f3c
xqkeji <xqkeji@foxmail.com>
2025-11-30 09:36:07 +0800

    修复表单自适应

--- src/form/template/default.php
+++ src/form/template/default.php
@@ -1,5 +1,5 @@
 <div class="form-group row align-items-center px-3">
-	<label class="col-2 col-md-2 col-form-label text-start text-md-end"><?=$text?></label>
+	<label class="col-12 col-md-2 col-form-label text-start text-md-end"><?=$text?></label>
 	<div class="col-12 col-md-4 mt-2 mt-md-0">
 		<?=$content?>
 	</div>
