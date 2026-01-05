ec6cf530e9d92486ea2a082214e7d49d90fb8599
xqkeji <xqkeji@foxmail.com>
2026-01-05 10:48:27 +0800

    搜索框右对齐

--- src/form/template/search.php
+++ src/form/template/search.php
@@ -1,4 +1,4 @@
-<div class="form-group d-flex align-items-center flex-grow-1">
+<div class="form-group d-flex align-items-center justify-content-end flex-grow-1">
   <label class="me-2 col-form-label text-sm"><?=$text?></label>
   <?=$content?>
 </div>
