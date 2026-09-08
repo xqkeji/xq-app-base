f024def61f9573cc7d16e17cf04d674a6bc35738
xqkeji <xqkeji@foxmail.com>
2026-09-08 16:53:03 +0800

    添加一个SwitchCheck的别名Status

---
+++ src/form/element/Status.php
@@ -0,0 +1,7 @@
+<?php
+namespace xqkeji\app\base\form\element;
+use xqkeji\app\base\form\element\SwitchCheck;
+class Status extends SwitchCheck
+{
+    
+}
---
+++ src/table/element/Status.php
@@ -0,0 +1,8 @@
+<?php
+namespace xqkeji\app\base\table\element;
+use xqkeji\app\base\table\element\SwitchCheck;
+
+class Status extends SwitchCheck
+{
+    
+}
