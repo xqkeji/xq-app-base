338e5cf9101131a3313a1fd416a38c955fa09a5d
xqkeji <xqkeji@foxmail.com>
2026-09-20 11:37:34 +0800

    添加开始日期和结束日期

---
+++ src/form/element/EndDate.php
@@ -0,0 +1,8 @@
+<?php
+namespace xqkeji\app\base\form\element;
+use xqkeji\app\base\form\element\Date;
+class EndDate extends Date
+{
+    protected $name = 'end_date';
+    protected $text = '结束日期';
+}
---
+++ src/form/element/StartDate.php
@@ -0,0 +1,8 @@
+<?php
+namespace xqkeji\app\base\form\element;
+use xqkeji\app\base\form\element\Date;
+class StartDate extends Date
+{
+    protected $name = 'start_date';
+    protected $text = '开始日期';
+}
